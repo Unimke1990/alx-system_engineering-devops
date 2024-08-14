b8e2edcff6eea7b1ec64fedc8c468871d8400bc0
Agim Unimke Agba <agimunimke1990@gmail.com>
2024-08-14 05:38:53 -0700

    puppet script that creates a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,9 +2,9 @@
 
 file{
 '/tmp/school':
-ensure	=> 'present',
-mode => '0744',
-owner => 'www-data',
-group => 'www-data',
-content => ' I love Puppet'
+ ensure	=> 'present',
+ mode => '0744',
+ owner => 'www-data',
+ group => 'www-data',
+ content => ' I love Puppet'
 }
